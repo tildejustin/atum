bb803c1af370c0d49a2b3f6d645b72289f397ae4
tildejustin <spacepepper3.14@gmail.com>
2024-01-23 19:33:54 -0500

    a couple things I noticed from 1.15 porting

--- src/main/java/me/voidxwalker/autoreset/mixin/hotkey/MinecraftClientMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/hotkey/MinecraftClientMixin.java
@@ -2,15 +2,17 @@ package me.voidxwalker.autoreset.mixin.hotkey;
 
 import me.voidxwalker.autoreset.Atum;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.*;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.options.GameOptions;
+import net.minecraft.client.util.Window;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.text.*;
 import net.minecraft.util.profiler.ProfileResult;
 import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -38,9 +40,18 @@ public abstract class MinecraftClientMixin {
     @Shadow
     protected abstract boolean shouldMonitorTickDuration();
 
+    @Shadow
+    @Final
+    public Keyboard keyboard;
+
+    @Shadow
+    @Final
+    private Window window;
+
     @Inject(method = "startIntegratedServer(Ljava/lang/String;Lnet/minecraft/util/registry/RegistryTracker$Modifiable;Ljava/util/function/Function;Lcom/mojang/datafixers/util/Function4;ZLnet/minecraft/client/MinecraftClient$WorldLoadAction;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/integrated/IntegratedServer;isLoading()Z", shift = At.Shift.AFTER))
     private void resetPreview(CallbackInfo ci) {
         if (Atum.isResetScheduled() && FabricLoader.getInstance().isModLoaded("worldpreview")) {
+            this.keyboard.onKey(this.window.getHandle(), GLFW.GLFW_KEY_ESCAPE, 1, 0, 0);
             this.clickButton(this.currentScreen, "menu.returnToMenu");
         }
     }
@@ -51,7 +62,7 @@ public abstract class MinecraftClientMixin {
             if (this.world != null) {
                 Screen gameMenuScreen = new GameMenuScreen(true);
                 gameMenuScreen.init((MinecraftClient) (Object) this, 0, 0);
-                if (!this.clickButton(gameMenuScreen, "fast_reset.menu.quitWorld", "menu.returnToMenu", "menu.disconnect") || this.world != null) {
+                if (!this.clickButton(gameMenuScreen, "fast_reset.menu.quitWorld", "menu.quitWorld", "menu.returnToMenu", "menu.disconnect") || this.world != null) {
                     if (this.world != null) {
                         this.world.disconnect();
                         this.disconnect(new SaveLevelScreen(new TranslatableText("menu.savingLevel")));
