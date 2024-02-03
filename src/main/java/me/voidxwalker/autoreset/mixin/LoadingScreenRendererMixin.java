daff2830ed83f126da58316c9d50b9b4ca160fa8
tildejustin <spacepepper3.14@gmail.com>
2024-02-02 20:39:21 -0500

    fix seed rendering after joining world

--- src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
@@ -1,18 +1,27 @@
 package me.voidxwalker.autoreset.mixin;
 
 import me.voidxwalker.autoreset.Atum;
-import net.minecraft.client.gui.screen.*;
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.*;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ProgressScreen.class)
-public class LoadingScreenRendererMixin extends Screen {
-    @Inject(method = "method_2214", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ProgressScreen;method_1789(Lnet/minecraft/class_0_681;Ljava/lang/String;III)V", ordinal = 0, shift = At.Shift.AFTER))
-    public void modifyString(int mouseX, int mouseY, float tickDelta, CallbackInfo ci) {
-        if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty()) {
+@Mixin(class_0_675.class)
+public class LoadingScreenRendererMixin {
+    @Shadow
+    @Final
+    private class_0_686 field_0_2786;
+
+    @Shadow @Final private MinecraftClient field_0_2782;
+
+    @Inject(method = "progressStagePercentage", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_0_681;method_0_2382(Ljava/lang/String;FFI)I", ordinal = 1, shift = At.Shift.AFTER))
+    public void renderSeed(int percentage, CallbackInfo ci) {
+        if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty() && MinecraftClient.getInstance().getServer() != null && MinecraftClient.getInstance().world == null) {
+            int j = this.field_0_2786.method_0_2459();
+            int k = this.field_0_2786.method_0_2460();
             String string = Atum.seed;
-            this.method_1789(this.field_2554, string, this.field_2561 / 2, 50, 16777215);
+            this.field_0_2782.field_1772.method_0_2382(string, (float) (j - this.field_0_2782.field_1772.method_0_2381(string)) / 2, (float) k / 2 - 4 - 40, 0xFFFFFF);
         }
     }
 }
