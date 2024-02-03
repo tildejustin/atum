d89a6a7cabbe4f1cb39b6e62aa45f79775597ffc
tildejustin <spacepepper3.14@gmail.com>
2024-02-02 20:39:21 -0500

    fix seed rendering after joining world

--- src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
@@ -1,18 +1,30 @@
 package me.voidxwalker.autoreset.mixin;
 
 import me.voidxwalker.autoreset.Atum;
-import net.minecraft.client.gui.screen.*;
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.LoadingScreenRenderer;
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ProgressScreen.class)
-public class LoadingScreenRendererMixin extends Screen {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ProgressScreen;drawCenteredString(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V", ordinal = 0, shift = At.Shift.AFTER))
-    public void modifyString(int mouseX, int mouseY, float tickDelta, CallbackInfo ci) {
-        if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty()) {
+@Mixin(LoadingScreenRenderer.class)
+public class LoadingScreenRendererMixin {
+    @Shadow
+    @Final
+    private Window window;
+
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Inject(method = "setProgressPercentage", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Ljava/lang/String;FFI)I", ordinal = 1, shift = At.Shift.AFTER))
+    public void renderSeed(int percentage, CallbackInfo ci) {
+        if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty() && MinecraftClient.getInstance().getServer() != null && MinecraftClient.getInstance().world == null) {
+            int j = this.window.getWidth();
+            int k = this.window.getHeight();
             String string = Atum.seed;
-            this.drawCenteredString(this.textRenderer, string, this.width / 2, 50, 16777215);
+            this.client.textRenderer.drawWithShadow(string, (float) (j - this.client.textRenderer.getStringWidth(string)) / 2, (float) k / 2 - 4 - 40, 0xFFFFFF);
         }
     }
 }
