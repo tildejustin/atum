31f52553d8a49c4f571bc0e1067e53c156a107ea
tildejustin <spacepepper3.14@gmail.com>
2024-02-02 20:42:11 -0500

    fix seed rendering after joining world

--- src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
@@ -1,6 +1,7 @@
 package me.voidxwalker.autoreset.mixin;
 
 import me.voidxwalker.autoreset.Atum;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
@@ -10,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class LoadingScreenRendererMixin extends Screen {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ProgressScreen;drawCenteredString(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V", ordinal = 0, shift = At.Shift.AFTER))
     public void modifyString(int mouseX, int mouseY, float tickDelta, CallbackInfo ci) {
-        if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty()) {
+        if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty() && Atum.isLoadingWorld()) {
             String string = Atum.seed;
             this.drawCenteredString(this.textRenderer, string, this.width / 2, 50, 16777215);
         }
