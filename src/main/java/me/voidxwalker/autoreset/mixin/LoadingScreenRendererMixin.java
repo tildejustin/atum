1b6ae7248e370532397d2acf0fc92611451fc6e0
tildejustin <spacepepper3.14@gmail.com>
2024-02-02 20:23:56 -0500

    fix seed being considered set when 0

--- src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/LoadingScreenRendererMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ProgressScreen.class)
 public class LoadingScreenRendererMixin extends Screen {
-
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ProgressScreen;drawCenteredString(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V", ordinal = 0, shift = At.Shift.AFTER))
     public void modifyString(int mouseX, int mouseY, float tickDelta, CallbackInfo ci) {
         if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty()) {
