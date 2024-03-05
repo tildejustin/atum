7d237579e6414509a18b122a3faa5ebaf3b9eace
tildejustin <spacepepper3.14@gmail.com>
2024-03-05 00:21:20 -0500

    remove unnecessary difficulty code

--- src/main/java/me/voidxwalker/autoreset/mixin/TitleScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/TitleScreenMixin.java
@@ -43,15 +43,12 @@ public class TitleScreenMixin extends Screen {
         this.minecraft.getTextureManager().bindTexture(BUTTON_IMAGE);
         blit(this.width / 2 - 124 + 2, this.height / 4 + 48 + 2, 0.0F, 0.0F, 16, 16, 16, 16);
         if (resetButton.isHovered() && hasShiftDown()) {
-            drawCenteredString(minecraft.textRenderer, getDifficultyText().asString(), this.width / 2 - 124 + 11, this.height / 4 + 48 - 15, 16777215);
+            drawCenteredString(minecraft.textRenderer, getDifficultyText().getString(), this.width / 2 - 124 + 11, this.height / 4 + 48 - 15, 16777215);
         }
     }
 
     @Unique
     Text getDifficultyText() {
-        if (Atum.difficulty == -1) {
-            return new TranslatableText("selectWorld.gameMode.hardcore");
-        }
-        return Difficulty.byOrdinal(Atum.difficulty).getTranslatableName();
+        return new TranslatableText("selectWorld.gameMode.hardcore").append(": ").append(new TranslatableText("options." + (Atum.difficulty != -1 ? "off" : "on")));
     }
 }
