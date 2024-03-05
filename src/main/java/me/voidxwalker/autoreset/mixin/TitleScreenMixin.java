2444cbc99203f6e24f02c539fcc83704f329067c
tildejustin <spacepepper3.14@gmail.com>
2024-03-05 00:21:20 -0500

    remove unnecessary difficulty code

--- src/main/java/me/voidxwalker/autoreset/mixin/TitleScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/TitleScreenMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.text.*;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
@@ -15,9 +16,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class TitleScreenMixin extends Screen {
     @Unique
     private static final Identifier BUTTON_IMAGE = new Identifier("textures/items/gold_boots.png");
-
-    @Unique
-    private String difficulty;
 
     @Inject(method = "method_2224", at = @At("TAIL"))
     private void init(CallbackInfo info) {
@@ -31,11 +29,10 @@ public abstract class TitleScreenMixin extends Screen {
 
     @Inject(method = "method_2214", at = @At("TAIL"))
     private void goldBootsOverlay(int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        getDifficulty();
         this.field_2563.getTextureManager().bindTextureInner(BUTTON_IMAGE);
         method_1781(this.field_2561 / 2 - 124 + 2, this.field_2559 / 4 + 48 + 2, 0.0F, 0.0F, 16, 16, 16, 16);
         if (mouseX > this.field_2561 / 2 - 124 && mouseX < this.field_2561 / 2 - 124 + 20 && mouseY > this.field_2559 / 4 + 48 && mouseY < this.field_2559 / 4 + 48 + 20 && method_2223()) {
-            method_1789(field_2563.field_1772, difficulty, this.field_2561 / 2 - 124 + 11, this.field_2559 / 4 + 48 - 15, 16777215);
+            method_1789(field_2563.field_1772, getDifficultyText().method_10865(), this.field_2561 / 2 - 124 + 11, this.field_2559 / 4 + 48 - 15, 16777215);
         }
     }
 
@@ -53,11 +50,7 @@ public abstract class TitleScreenMixin extends Screen {
     }
 
     @Unique
-    private void getDifficulty() {
-        if (Atum.difficulty == -1) {
-            difficulty = "Hardcore: ON";
-        } else {
-            difficulty = "Hardcore: OFF";
-        }
+    Text getDifficultyText() {
+        return new TranslatableTextContent("selectWorld.gameMode.hardcore").append(": ").append(new TranslatableTextContent("options." + (Atum.difficulty != -1 ? "off" : "on")));
     }
 }
