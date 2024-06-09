da7d7aa6b82d4537616bda601b5f647a6bbaca8c
tildejustin <spacepepper3.14@gmail.com>
2024-06-08 22:13:23 -0400

    stop resetting from death screen title button

---
+++ src/main/java/me/voidxwalker/autoreset/mixin/DeathScreenMixin.java
@@ -0,0 +1,15 @@
+package me.voidxwalker.autoreset.mixin;
+
+import me.voidxwalker.autoreset.Atum;
+import net.minecraft.client.gui.screen.DeathScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DeathScreen.class)
+public abstract class DeathScreenMixin {
+    @Inject(method = "method_0_2702", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;disconnect()V"))
+    private void stopResettingOnDeathQuit(CallbackInfo ci) {
+        Atum.isRunning = false;
+    }
+}
