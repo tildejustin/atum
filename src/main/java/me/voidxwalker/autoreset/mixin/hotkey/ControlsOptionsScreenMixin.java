f0715d31a71c6a35b61931caa00e85905788ca43
tildejustin <spacepepper3.14@gmail.com>
2024-07-02 11:29:47 -0400

    support legacy fabric api

--- src/main/java/me/voidxwalker/autoreset/mixin/hotkey/ControlsOptionsScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/hotkey/ControlsOptionsScreenMixin.java
@@ -1,5 +1,6 @@
 package me.voidxwalker.autoreset.mixin.hotkey;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +10,6 @@ import org.spongepowered.asm.mixin.injection.*;
 public abstract class ControlsOptionsScreenMixin extends Screen {
     @ModifyArg(method = "init", index = 2, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;<init>(IIILjava/lang/String;)V"))
     private int dynamicDoneButton(int num) {
-        return this.height / 6 + 24 * (((this.client.options.allKeys.length + 2 - 1) / 2));
+        return !FabricLoader.getInstance().isModLoaded("legacy-fabric-api") ? this.height / 6 + 24 * ((this.client.options.allKeys.length + 2 - 1) / 2) : num;
     }
 }
