fa45f508c1f512e742b37c8e92e62113923df965
contaria <kingcontaria@gmail.com>
2024-08-14 15:56:28 +0200

    fix: support mapping reset key to a mouse button https://github.com/KingContaria/atum-rewrite/issues/2

--- src/main/java/me/voidxwalker/autoreset/mixin/hotkey/MouseMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/hotkey/MouseMixin.java
@@ -1,8 +1,8 @@
 package me.voidxwalker.autoreset.mixin.hotkey;
 
 import me.voidxwalker.autoreset.Atum;
-import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
 import net.minecraft.client.gui.screen.options.ControlsOptionsScreen;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,17 +11,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Keyboard.class)
-public abstract class KeyboardMixin {
+@Mixin(Mouse.class)
+public abstract class MouseMixin {
     @Shadow
     @Final
     private MinecraftClient client;
 
-    // injecting at Keyboard#debugCrashStartTime ensures the window handle check has succeeded
-    @Inject(method = "onKey", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Keyboard;debugCrashStartTime:J", ordinal = 0), cancellable = true)
-    private void onKey(long window, int key, int scancode, int action, int j, CallbackInfo ci) {
+    // injecting at MinecraftClient#IS_SYSTEM_MAC ensures the window handle check has succeeded
+    @Inject(method = "onMouseButton", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;IS_SYSTEM_MAC:Z", ordinal = 0), cancellable = true)
+    private void onKey(long window, int button, int action, int mods, CallbackInfo ci) {
         // 1 is GLFW for "clicked" (0 -> "released", 2 -> "held down")
-        if (action == 1 && Atum.resetKey.matchesKey(key, scancode)) {
+        if (action == 1 && Atum.resetKey.matchesMouse(button)) {
             if (this.client.currentScreen instanceof ControlsOptionsScreen && ((ControlsOptionsScreen) this.client.currentScreen).focusedBinding == Atum.resetKey) {
                 return;
             }
