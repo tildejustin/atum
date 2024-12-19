9faac5891b101509a305579a5b1703a8808ad5ed
tildejustin <spacepepper3.14@gmail.com>
2024-12-19 10:24:42 -0500

    add a space above atum lines and display atum difficulty, bump version to 1.3.1

--- src/main/java/me/voidxwalker/autoreset/mixin/DebugHudMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/DebugHudMixin.java
@@ -3,6 +3,7 @@ package me.voidxwalker.autoreset.mixin;
 import me.voidxwalker.autoreset.Atum;
 import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.text.Text;
+import net.minecraft.world.Difficulty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,7 +16,8 @@ public class DebugHudMixin {
     private void getRightText(CallbackInfoReturnable<List<String>> info) {
         if (Atum.isRunning) {
             List<String> returnValue = info.getReturnValue();
-            returnValue.add("Resetting " + (Atum.seed == null || Atum.seed.isEmpty() ? "a random seed" : ("the seed: \"" + Atum.seed + "\"")));
+            returnValue.add("");
+            returnValue.add("Resetting " + (Atum.seed == null || Atum.seed.isEmpty() ? "a random seed" : "the seed: \"" + Atum.seed + "\"") + ", " + (Atum.difficulty != -1 ? "" + Difficulty.byOrdinal(Atum.difficulty).getName().charAt(0) : "hc"));
             if (Atum.generatorType != 0) {
                 String s = Atum.getGeneratorTypeString(Atum.generatorType);
                 if (s != null) {
