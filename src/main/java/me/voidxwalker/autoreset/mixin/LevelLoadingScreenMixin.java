dc8d504172ff97fb551f56e2467809c6135ff894
tildejustin <spacepepper3.14@gmail.com>
2025-06-21 19:23:05 -0400

    1.3.2: fix seed text on lls

--- src/main/java/me/voidxwalker/autoreset/mixin/LevelLoadingScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/LevelLoadingScreenMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.LevelLoadingScreen;
 import net.minecraft.text.Text;
+import net.minecraft.util.Colors;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
@@ -19,7 +20,7 @@ public class LevelLoadingScreenMixin extends Screen {
     public void modifyString(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci, int i, int j) {
         if (Atum.isRunning && Atum.seed != null && !Atum.seed.isEmpty()) {
             String string = Atum.seed;
-            context.drawCenteredTextWithShadow(this.textRenderer, string, i, j - 9 / 2 - 50, 16777215);
+            context.drawCenteredTextWithShadow(this.textRenderer, string, i, j - 9 / 2 - 50, Colors.WHITE);
         }
     }
 }
