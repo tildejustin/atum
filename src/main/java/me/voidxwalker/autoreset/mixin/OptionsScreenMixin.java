8d520d493d3cb2341ac1efc5f90820557999a543
tildejustin <spacepepper3.14@gmail.com>
2025-06-21 19:23:27 -0400

    1.3.3: fix stop resetting button moving when resizing window

--- src/main/java/me/voidxwalker/autoreset/mixin/OptionsScreenMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/OptionsScreenMixin.java
@@ -5,28 +5,36 @@ import net.minecraft.client.gui.screen.*;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(OptionsScreen.class)
 public class OptionsScreenMixin extends Screen {
+    @Unique
+    ButtonWidget stopResetting;
+
     protected OptionsScreenMixin(Text title) {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
+    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/option/OptionsScreen;initTabNavigation()V"))
     public void addAutoResetButton(CallbackInfo ci) {
         if (Atum.isRunning) {
-            this.addDrawableChild(ButtonWidget.builder(Atum.getTranslation("menu.stop_resets", "Stop Resets & Quit"), (buttonWidget) -> {
+            stopResetting = this.addDrawableChild(ButtonWidget.builder(Atum.getTranslation("menu.stop_resets", "Stop Resets & Quit"), (buttonWidget) -> {
                 Atum.isRunning = false;
                 if (this.client != null && this.client.world != null) {
                     this.client.world.disconnect();
                     this.client.disconnect(new MessageScreen(Text.translatable("menu.savingLevel")));
                     this.client.setScreen(new TitleScreen());
                 }
-            }).dimensions(0, this.height - 20, 100, 20).build());
+            }).size(100, 20).build());
         }
+    }
+
+    @Inject(method = "initTabNavigation", at = @At("TAIL"))
+    private void moveAutoResetButton(CallbackInfo ci) {
+        stopResetting.setPosition(0, this.height - 20);
     }
 }
