de198fdcb1009f2ca2222810dc6adb6c18e1d659
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-10-03 18:18:13 -0300

    Change F3 implementation

--- src/main/java/me/voidxwalker/autoreset/mixin/DebugHudMixin.java
+++ src/main/java/me/voidxwalker/autoreset/mixin/DebugHudMixin.java
@@ -1,37 +1,37 @@
 package me.voidxwalker.autoreset.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import me.voidxwalker.autoreset.Atum;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.client.gui.hud.debug.DebugHudLines;
 import net.minecraft.text.Text;
 import net.minecraft.world.Difficulty;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.List;
 
 @Mixin(DebugHud.class)
 public abstract class DebugHudMixin {
     // We could add an actual debug category, but that makes the section disableable, and involves a bunch more work to prevent that.
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;getWorld()Lnet/minecraft/world/World;", shift = At.Shift.AFTER))
-    private void addDebugInfo(DrawContext context, CallbackInfo ci, @Local DebugHudLines lines) {
-        if (Atum.isRunning && MinecraftClient.getInstance().debugHudEntryList.isF3Enabled()) {
-            lines.addLineToSection(Atum.DEBUG_SECTION_IDENTIFIER, "Resetting " + (Atum.seed == null || Atum.seed.isEmpty() ? "a random seed" : "the seed: \"" + Atum.seed + "\"") + ", " + (Atum.difficulty != -1 ? "" + Difficulty.byId(Atum.difficulty).getName().charAt(0) : "hc"));
-            if (Atum.generatorType != 0) {
-                String s = Atum.getGeneratorTypeString(Atum.generatorType);
-                if (s != null) {
-                    lines.addLineToSection(Atum.DEBUG_SECTION_IDENTIFIER, Text.literal("GenType: ").append(s).getString());
-                }
-            }
-            if (!Atum.structures) {
-                lines.addLineToSection(Atum.DEBUG_SECTION_IDENTIFIER, "NoStructures");
-            }
-            if (Atum.bonusChest) {
-                lines.addLineToSection(Atum.DEBUG_SECTION_IDENTIFIER, "BonusChest");
+    @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;drawText(Lnet/minecraft/client/gui/DrawContext;Ljava/util/List;Z)V", ordinal = 1))
+    private List<String> addDebugInfo(List<String> lines) {
+        if (!(Atum.isRunning && MinecraftClient.getInstance().debugHudEntryList.isF3Enabled())) return lines;
+
+        if (!lines.isEmpty() && !lines.getLast().isEmpty()) lines.add("");
+        lines.add("Resetting " + (Atum.seed == null || Atum.seed.isEmpty() ? "a random seed" : "the seed: \"" + Atum.seed + "\"") + ", " + (Atum.difficulty != -1 ? "" + Difficulty.byId(Atum.difficulty).getName().charAt(0) : "hc"));
+        if (Atum.generatorType != 0) {
+            String s = Atum.getGeneratorTypeString(Atum.generatorType);
+            if (s != null) {
+                lines.add(Text.literal("GenType: ").append(s).getString());
             }
         }
+        if (!Atum.structures) {
+            lines.add("NoStructures");
+        }
+        if (Atum.bonusChest) {
+            lines.add("BonusChest");
+        }
+        return lines;
     }
 }
